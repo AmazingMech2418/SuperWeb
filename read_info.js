aa8de8cd27eda4ad44d77bade49112afa45cb9dd
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-07-14 10:57:49 -0400

    Update read_info.js

--- read_info.js
+++ read_info.js
@@ -1,1 +1,12 @@
 
+$.noConflict
+            function apiLib(url) {
+                var rtrn
+             jQuery.ajax({async:false,url: url, success: function(result){
+                if(url.search(".json")!==-1) {rtrn= JSON.parse(result);} else if(url.search(".js")!==-1) {rtrn= eval("function() {"+result+"}");} else {
+            rtrn= result;
+                }
+        }});return rtrn;
+            }
+var config = apiLib("config.json");
+var name = location.protocol+location.host+location.pathname;
